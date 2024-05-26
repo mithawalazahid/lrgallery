ad2db90d4547b488c4dff8b453ee734d09d2e502
zahid <zahidmemon14@gmail.com>
2024-05-26 20:45:06 +0530

    update 26may

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,useEffect } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count , setcount] =useState(0);
 
+  const [form, setform] = useState({ phone: "" })
+
+
+
+  const handleChange = (e) => {
+    setform({ ...form, [e.target.name]: e.target.value })
+  }
+
+  
+    
+    const phone = form.phone;
+    const web = "https://wa.me/" + "+91"+phone
+    
+    
+    function searchURL() {
+
+      console.log(web)
+     alert("testing")
+  }
+    
+    
   return (
     <>
-      <div>this is count:{count}</div>
-      <button onClick={()=>{setcount(count+1)}}>click me</button>
+      
+     <h1>whatapp direct chat</h1>
+    <div className='gap'>
+
+    </div>
+    <input value={form.phone} onChange={handleChange} placeholder='Enter number' className='rounded-full  border border-green-500 w-full p-4 py-1' type="tel" pattern="[0-9]"name='phone' id='phone' required />
+     <a href={web}>open</a>
+
+    
+     
     </>
   )
 }
