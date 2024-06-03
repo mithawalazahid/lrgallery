c60e70ad82f8f0fd9cea0683bbcbee6b49b20b70
zahid <zahidmemon14@gmail.com>
2024-06-03 23:57:31 +0530

    update design

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
 import './App.css'
 
@@ -7,20 +7,23 @@ function App() {
   const handleChange = (e) => {
     setform({ ...form, [e.target.name]: e.target.value })
   }
-    const phone = form.phone;
-    const web = "https://wa.me/" + "+91"+phone 
+  const phone = form.phone;
+  const web = "https://wa.me/" + "+91" + phone
   return (
     <>
-      
-     <h1 className='text-gray-600'>whatapp direct chat</h1>
-    <div className='gap'>
 
-    </div>
-    <input value={form.phone} onChange={handleChange} placeholder='Enter number' className='rounded-full  border border-green-500 w-full p-4 py-1' type="tel" pattern="[0-9]"name='phone' id='phone' required />
-     <a href={web}>open</a>
+      <h1 className='text-green-700 font-bold text-3xl py-2'>whatapp direct chat</h1>
+      <div className='flex justify-center py-2'>
+        <p className='text-gray-500 py-4 w-[90%] text-xl'>now u can message to any person without saving his number in your contact list,just enter the number in input box and start chat</p>
+      </div>
 
-    
-     
+      <div className='flex justify-center gap-3'>
+      <input value={form.phone} onChange={handleChange} placeholder='Enter number' className='rounded-md  border border-green-500  p-4 py-1' type="tel" pattern="[0-9]" name='phone' id='phone' required />
+      <a className='text-green-700 font-bold ' href={web}>open</a>
+      </div>
+
+
+
     </>
   )
 }
